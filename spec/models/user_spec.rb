d92b74e5338b20d0a85581ac04daab5e36922249
Huawei Weng <hweng@ucsd.edu>
2014-12-12 10:56:40 -0800

    Adding feature spec for social networking feature.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -28,13 +28,13 @@ describe "#follows?" do
     alice = Fabricate(:user)
     bob = Fabricate(:user)
     Fabricate(:relationship, leader: bob, follower: alice)
-    expect(alice.follows?(bob)).to be_true
+    expect(alice.follows?(bob)).to be_truthy
   end
 
   it "returns false if the user does not have a following relationship with another user" do
     alice = Fabricate(:user)
     bob = Fabricate(:user)
     Fabricate(:relationship, leader: alice, follower: bob)
-    expect(alice.follows?(bob)).to be_false
+    expect(alice.follows?(bob)).to be_falsey
   end
 end
